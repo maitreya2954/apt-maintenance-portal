018e5c5d5dc6b0d54720b2fba13fea7abfdbfda0
Siddharth Rayabharam <maitreya2954@gmail.com>
2022-12-14 00:05:19 -0500

    Login created

--- skyway-management-2/src/pages/Login.js
+++ skyway-management-2/src/pages/Login.js
@@ -1,38 +1,56 @@
 import React from "react";
+import $ from 'jquery';
 
-class Login extends React.Component
-{
-    render()
-   {
-     const styleObj1 = {
-        fontSize: 40,
-        fontWeight:700,
-    
-     }
-   
-     return(
+class Login extends React.Component {
+    render() {
+        const styleObj1 = {
+            fontSize: 40,
+            fontWeight: 700,
 
-    
-            <div class="page-body">
-                 <img id="background-img" alt="" src="D:\skyway-management-2\src\pages\images\brandon-griggs-wR11KBaB86U-uns.png"/> 
-                <form id="login-container">
+        }
+        return (
+
+
+            <div className="page-body">
+                <img id="background-img" alt="" src="D:\skyway-management-2\src\pages\images\brandon-griggs-wR11KBaB86U-uns.png" />
+                <form name='loginform' id="login-container" onSubmit={(e) => {
+                    e.preventDefault();
+                    var inputs = document.getElementsByTagName("input");
+                    for (var i = 0; i < inputs.length; i++) {
+                        inputs[i].disabled = false;
+                    }
+                    var formdata = $('#login-container').serialize();
+                    $.ajax({
+                        url: 'http://localhost:8080/portal/login?' + formdata,
+                        type: 'GET',
+                        success: (resp) => {
+                            localStorage.setItem(resp.username+'-data', resp);
+                            if (resp.role == 0) {
+                                window.location="/tenant";
+                            } else if(resp.role == 1) {
+                                window.location='/manager';
+                            } else{
+                                window.location='/team';
+                            }
+                        }
+                    });
+                }}>
                     <span style={styleObj1}/*"font-size: 40px; font-weight: 700;"*/>Skyway Portal</span>
                     <div id="divider-line"></div>
                     <div id="sub-login-container">
-                        <span>Email</span>
-                        <input type="email"/>
+                        <span>User ID</span>
+                        <input type="text" name="userid" />
                     </div>
                     <div id="sub-login-container">
                         <span>Password</span>
-                        <input type="password"/>
+                        <input type="password" name="pass" />
                     </div>
-                    <input id="login-button" type="submit" value="Login"/>
+                    <input id="login-button" name="submit" type="submit" value="Login" />
                 </form>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    
+
             </div>
-        
-);
-}
+
+        );
+    }
 }
 export default Login;
