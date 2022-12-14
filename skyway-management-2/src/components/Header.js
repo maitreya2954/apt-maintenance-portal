392430f4a098df6fcdf2a1206127c21cc839a870
Siddharth Rayabharam <maitreya2954@gmail.com>
2022-12-13 23:02:33 -0500

    fixes

--- skyway-management-2/src/components/Header.js
+++ skyway-management-2/src/components/Header.js
@@ -1,32 +1,41 @@
 import React from "react";
-import { NavLink,Link} from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
-class Header extends React.Component
-{
-    
-    
-    render()
-   {
-    
-     
-     return(
+class Header extends React.Component {
 
-        
-        <header>
-            <div class="page-header">
-                <img id="skywaylogo"  src="..\pages\images\Screenshot_2022-11-24_at_90258.png"/>
-                <div class="header-button-container">
-                    <button class="headerbutton"Link to="/contact">Contact Us 
-                    
-                    </button>
-                    <button class="headerbutton">About Us</button>
-                    <button class="headerbutton">Login</button>
-                    
+    contactUs() {
 
+    }
+
+    render() {
+
+
+        return (
+
+
+            <header>
+                <div class="page-header">
+                    <img id="skywaylogo" src={require("../pages/images/Screenshot_2022-11-24_at_90258.png")} />
+                    <div class="header-button-container">
+
+                        <button class="headerbutton" onClick={(e) => {
+                            e.preventDefault();
+                            window.location = "/contact";
+                        }}>Contact Us</button>
+                        <button class="headerbutton" onClick={(e) => {
+                            e.preventDefault();
+                            window.location="/about";
+                        }}>About Us</button>
+                        <button class="headerbutton" onClick={(e) => {
+                            e.preventDefault();
+                            window.location="/login";
+                        }}>Login</button>
+
+
+                    </div>
                 </div>
-            </div>
             </header>
-);
-}
+        );
+    }
 }
 export default Header;
